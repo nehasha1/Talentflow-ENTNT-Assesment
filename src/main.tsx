1245911391f2d325152a16cdd8740dff9d0f7a4e
Ramanchoudhary07 <ramantarar1@gmail.com>
2025-09-13 11:11:55 +0530

    (client): Initialize mock server and database

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,25 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { initializeJobs } from "./services/db/jobsDb";
+import { initializeCandidates } from "./services/db/candidatesDb";
+import { initializeAssessments } from "./services/db/assessmentsDb";
+
+if (process.env.NODE_ENV === "development") {
+  import("./services/mocks/browser").then(({ worker }) => {
+    worker
+      .start({
+        onUnhandledRequest: "warn",
+      })
+      .then(() => {
+        console.log("MSW started");
+      });
+  });
+}
+
+initializeJobs();
+initializeCandidates();
+initializeAssessments();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
